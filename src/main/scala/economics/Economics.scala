a6e68483232503b7e874f5de98371201d9429b96
pierre-alain9 <78470866+pierre-alain9@users.noreply.github.com>
2022-05-12 09:47:44 +0200

    partE change name variable

--- src/main/scala/economics/Economics.scala
+++ src/main/scala/economics/Economics.scala
@@ -18,15 +18,32 @@ object Economics {
     println("******************************************************")
 
     var conf = new Conf(args)
-    val e1 :Double = (38600.0/20.40).ceil.toDouble
-    val e21:Double = (1.14e-6+1.6e-7*32) *3600*24
-    val e22:Double=4*3*0.25*24/1000
-    val e23:Double =4*4*0.25*24/1000
-    val e24:Double= (108.48/e22).ceil
-    val e25:Double = (108.48/e23).ceil
-    val e31:Double = (38600/108.48).floor
-    val e32:Double = 8/(e31*24*64)
-    val e33:Double = (2*14*2.6) / (e31*4*1.5)
+
+    val priceM7 = 38600.0
+    val rentingDay = 20.40
+    val vCPUs = 1.14e-6
+    val nbrVCPU = 1.0 
+    val cGBs = 1.6e-7
+    val nbGB = 32.0
+    val nbRPis = 4.0
+    val energyCost = 0.25
+    val priceRPI4 = 108.48
+    val ramICCM7 = 24.0 * 64.0
+    val ramRPIs = 8.0
+    val nbrCICCM7 = 2.0 * 14.0
+    val throughPutICCM7 = 2.6
+    val nbrCRPIs = 4.0
+    val throughPutRPIs = 1.5
+
+    val e1 :Double = (priceM7 / rentingDay).ceil.toDouble
+    val e21:Double = (vCPUs * nbrVCPU + cGBs * nbGB)*3600*24
+    val e22:Double= nbRPis * 3 * energyCost *24/1000
+    val e23:Double =nbRPis * 4 * energyCost *24/1000
+    val e24:Double= (priceRPI4 / e22).ceil
+    val e25:Double = (priceRPI4 / e23).ceil
+    val e31:Double = (priceM7 / priceRPI4).floor
+    val e32:Double = ramICCM7 / (e31 * ramRPIs)
+    val e33:Double = (nbrCICCM7 * throughPutICCM7) / (e31 * nbrCRPIs * throughPutRPIs)
     // Save answers as JSON
     def printToFile(content: String,
                     location: String = "./answers.json") =
